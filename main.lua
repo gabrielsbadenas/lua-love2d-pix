f24270bd121340f2d5c19a218e63514207488d24
gabriel <gabrielsbadenas@gmail.com>
2023-11-24 18:27:54 -0300

    tileset

--- main.lua
+++ main.lua
@@ -1,14 +1,18 @@
 PIX = require "PIX"
+map={{1,2},{3,4}}
+resolution = 768
 function love.load()
-    love.window.setMode(1366, 768, {resizable=true, vsync=true, minwidth=256, minheight=144})
+    love.window.setMode(1366, resolution, {resizable=true, vsync=true, minwidth=256, minheight=144})
 	love.window.setFullscreen(true)
     -- how to use: PIX.init(minimum width,minimum height, scaling mode)
 
     PIX.init(256,144,"floor") -- tries most optimal amount of pixels for the aspect ratio, pixels will all be equal size
     --PIX.init(128,128,"stretch") -- steches the pixels to fill the screen, pixels could be differnt size
-    --PIX.init(128,128,3) -- fixed mode, set the amount the pixels will scale, in this case 3x
-
+    --PIX.init(256,144,resolution/144) -- fixed mode, set the amount the pixels will scale, in this case 3x
     test = PIX.newImage("index.png") -- helper for images drawn inside PIX
+
+	tileset=love.graphics.newSpriteBatch(test,16*16)
+
 end
 
 function DrawPixelated()
